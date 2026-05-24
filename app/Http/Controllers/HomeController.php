e99849fd0a042c3eb5841da83e695deeb6775735
Andrew Kester <aikester@aikester.com>
2026-05-24 18:12:50 -0500

    Add controller fro home page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trade;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display the homepage with recent trades.
+     */
+    public function index()
+    {
+        $trades = Trade::orderBy('created_at', 'desc')->limit(50)->get();
+
+        return view('home', compact('trades'));
+    }
+}
