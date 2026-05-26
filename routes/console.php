16cbf65f7d6bf122b560f721c50b626fd8597459
Andrew Kester <aikester@aikester.com>
2026-05-26 15:05:20 -0500

    Add schedule for sell commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Console\Commands\CheckOrders;
+use App\Console\Commands\SellStorj;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command(SellStorj::class)->everyFifteenMinutes();
+Schedule::command(CheckOrders::class)->everyFifteenMinutes();
