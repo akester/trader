791904fd04de60700813cca852f7f933cc8139df
Andrew Kester <aikester@aikester.com>
2026-05-26 16:33:10 -0500

    Don't queue notifications

--- app/Notifications/TradeComplete.php
+++ app/Notifications/TradeComplete.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TradeComplete extends Notification implements ShouldQueue
+class TradeComplete extends Notification
 {
     use Queueable;
 
--- app/Notifications/TradeFailed.php
+++ app/Notifications/TradeFailed.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TradeFailed extends Notification implements ShouldQueue
+class TradeFailed extends Notification
 {
     use Queueable;
 
