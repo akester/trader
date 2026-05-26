d992857edef6a9861e8e509d30e8ce57c9ef72cb
Andrew Kester <aikester@aikester.com>
2026-05-25 22:30:03 -0500

    Add correct favicons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,10 @@
         <title>{{ config('app.name', 'Trader') }}</title>
 
         @vite(['resources/css/app.scss', 'resources/js/app.js'])
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     </head>
 
     <body>
@@ -14,7 +18,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <h1>{{ config('app.name', 'Trader') }}
+                        <h1>{{ config('app.name', 'Trader') }}</h1>
                     </div>
                 </div>
 
