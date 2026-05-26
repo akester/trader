e77af9c2d5f6943be448802b7aa769d52b3e4e09
Andrew Kester <aikester@aikester.com>
2026-05-26 11:19:40 -0500

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+# This confuses Laravel I think, we just flatten everything into one big file on the fly
+rm .env.prod
+
 php artisan config:clear
+php artisan cache:clear
 php artisan config:cache
 
 php artisan migrate --force
