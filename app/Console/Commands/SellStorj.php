e74e3c8ba2df2c0302f00598a5c8fd455201eec5
Andrew Kester <aikester@aikester.com>
2026-05-26 17:00:12 -0500

    Reduce balance precision again

--- app/Console/Commands/SellStorj.php
+++ app/Console/Commands/SellStorj.php
@@ -34,7 +34,7 @@ class SellStorj extends Command
             $balance = Coinbase::$MAX_TRADE_VOLUME;
         }
 
-        $balance = (float) round($balance, 3);
+        $balance = (float) round($balance, 2);
 
         // Check if we have an order in cooldown, this gives the first one time
         // to complete before we tell it to do another.
